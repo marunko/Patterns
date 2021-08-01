64a599ad9d17639c7d2b843fc65bc6426c5ae32e
marunko <48942512+marunko@users.noreply.github.com>
2021-08-01 14:30:23 +0300

    Update Prototype.java

--- CreationalDP/Prototype.java
+++ CreationalDP/Prototype.java
@@ -1,1 +1,83 @@
+abstract class Prototype_Transport{
+	abstract void move();
+	abstract void signal();
+}
 
+abstract class Proto_Ship extends Prototype_Transport{
+	abstract void swim();
+}
+abstract class Proto_Plane extends Prototype_Transport{
+	abstract void fly();
+}
+abstract class Proto_Vehicle extends Prototype_Transport{
+	abstract void drive();
+}
+
+class Bus extends Proto_Vehicle{
+
+	@Override
+	void drive() {
+
+	}
+
+	@Override
+	void move() {
+
+	}
+
+	@Override
+	void signal() {
+	
+	}
+	 
+}
+class Car extends Proto_Vehicle{
+	@Override
+	void drive() {
+
+	}
+
+	@Override
+	void move() {
+
+	}
+
+	@Override
+	void signal() {
+	
+	}
+}
+class Passenger_Plane extends Proto_Plane{
+
+	@Override
+	void fly() {
+ 	
+	}
+
+	@Override
+	void move() {
+ 
+	}
+
+	@Override
+	void signal() {
+ 
+	}
+}
+class Military_Jet extends Proto_Plane{
+
+	@Override
+	void fly() {
+ 	
+	}
+
+	@Override
+	void move() {
+ 
+	}
+
+	@Override
+	void signal() {
+
+	}
+}
